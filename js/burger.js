1b2fbfedc8cd96d484bb87b0068f305b045427f0
azad0313 <azad123@live.dk>
2025-02-03 08:47:23 +0100

    portfolio rettet, nogle projekter er fjernet og nye er tilføjet

--- js/burger.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const menuIcon = document.getElementById("menuIcon");
-  const navLinks = document.getElementById("navLinks");
-
-  menuIcon.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
-    menuIcon.classList.toggle("active");
-  });
-});
